0ec6d80fc59bee74f40e6b72042b0db6241deee4
cameron-ward <cameron.ward@vodafone.com>
2023-11-20 21:12:30 +0000

    Unit tested get SendMessageToGatewayActivity activity

--- workflows/communication_workflow/src/main/java/com/github/cameronward301/communication_scheduler/workflows/communication_workflow/activities/SendMessageToGatewayActivityImpl.java
+++ workflows/communication_workflow/src/main/java/com/github/cameronward301/communication_scheduler/workflows/communication_workflow/activities/SendMessageToGatewayActivityImpl.java
@@ -1,10 +1,10 @@
 package com.github.cameronward301.communication_scheduler.workflows.communication_workflow.activities;
 
+import com.github.cameronward301.communication_scheduler.workflows.communication_workflow.properties.ActivityProperties;
 import io.temporal.failure.ApplicationFailure;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.Map;
 
@@ -12,29 +12,38 @@ import java.util.Map;
 public class SendMessageToGatewayActivityImpl implements SendMessageToGatewayActivity {
 
     private final WebClient webClient;
+    private final ActivityProperties activityProperties;
 
-    public SendMessageToGatewayActivityImpl(WebClient webClient) {
+    public SendMessageToGatewayActivityImpl(WebClient webClient, ActivityProperties activityProperties) {
         this.webClient = webClient;
+        this.activityProperties = activityProperties;
     }
 
     @Override
     public Map<String, String> invokeGateway(String userId, String workflowRunId, String gatewayUrl) {
-        webClient.post()
-                .uri(gatewayUrl)
-                .bodyValue(Map.of("userId", userId, "workflowRunId", workflowRunId))
-                .retrieve()
-                .toEntity(String.class)
 
-                .subscribe(
-                        responseEntity -> {
-                            if (responseEntity.getStatusCode() != HttpStatusCode.valueOf(200)) {
-                                throw ApplicationFailure.newFailure("Gateway unsuccessful: " + responseEntity.getStatusCode(), "GatewayError");
-                            }
-                        },
-                        error -> {
-                            throw ApplicationFailure.newFailure("Could not invoke gateway", "GatewayError");
-                        }
-                );
+
+        try {
+            webClient.post()
+                    .uri(gatewayUrl)
+                    .bodyValue(Map.of("userId", userId, "workflowRunId", workflowRunId))
+                    .retrieve()
+                    .toEntity(String.class)
+                    .timeout(java.time.Duration.ofSeconds(activityProperties.getGateway_timeout_seconds()))
+                    .block();
+
+        } catch (WebClientResponseException e) {
+            log.error("Invalid Gateway response", e);
+            throw ApplicationFailure.newFailure("Gateway unsuccessful, status: " + e.getStatusCode().value() + " from: " + gatewayUrl, "GatewayError");
+        } catch (Exception e) {
+            if (e.getCause() instanceof java.util.concurrent.TimeoutException) {
+                log.error("Gateway timed out", e);
+                throw ApplicationFailure.newFailure("Gateway timed out: " + gatewayUrl, "GatewayError");
+            }
+            log.error("Could not invoke gateway", e);
+            throw ApplicationFailure.newFailure("Could not invoke gateway: " + gatewayUrl, "GatewayError");
+        }
+
 
         return Map.of("status", "complete", "userId", userId, "gateway_url", gatewayUrl);
     }
