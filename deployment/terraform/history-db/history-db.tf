4ae68e572354ab8fa7f9791adcf60809a0b72b43
cameron-ward <cameron.ward@vodafone.com>
2023-12-12 21:00:01 +0000

    added history database

---
+++ deployment/terraform/history-db/history-db.tf
@@ -0,0 +1,19 @@
+# Resource configured from: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "history_table" {
+  name                        = "history-db-${var.account_name}-${data.aws_caller_identity.current.account_id}-${var.region}"
+  billing_mode                = var.billing_mode
+  deletion_protection_enabled = var.deletion_protection_enabled
+  read_capacity               = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity              = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+  hash_key                    = "messageHash"
+
+  attribute {
+    name = "messageHash"
+    type = "S"
+  }
+
+  tags = merge(
+    var.default_tags,
+    { Name = "history-db-${var.account_name}-${data.aws_caller_identity.current.account_id}-${var.region}" }
+  )
+}
