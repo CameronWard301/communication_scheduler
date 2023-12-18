114d479dcb739f978e9119711d450506c2a910a3
cameron-ward <cameron.ward@vodafone.com>
2023-12-18 17:12:52 +0000

    init sms gateway

---
+++ sms-gateway/src/test/java/io/github/cameronward301/communication_scheduler/sms_gateway/SmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cameronward301.communication_scheduler.sms_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
