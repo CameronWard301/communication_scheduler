78d4013b0a4d3b30a949cb1e5d3f06a44fdeb718
cameron-ward <cameron.ward@vodafone.com>
2024-02-03 11:33:38 +0000

    use data encoder in integration tests

--- integration-tests/src/test/java/io/github/cameronward301/communication_scheduler/integration_tests/configuration/TemporalConfiguration.java
+++ integration-tests/src/test/java/io/github/cameronward301/communication_scheduler/integration_tests/configuration/TemporalConfiguration.java
@@ -1,31 +1,40 @@
 package io.github.cameronward301.communication_scheduler.integration_tests.configuration;
 
-import io.github.cameronward301.communication_scheduler.integration_tests.properties.TemporalProperties;
+import io.github.cameronward301.communication_scheduler.integration_tests.properties.IntegrationTestTemporalProperties;
+import io.github.cameronward301.communication_scheduler.workflows.communication_workflow.codec.CryptographyCodec;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowClientOptions;
+import io.temporal.common.converter.CodecDataConverter;
+import io.temporal.common.converter.DefaultDataConverter;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 @RequiredArgsConstructor
 public class TemporalConfiguration {
-    private final TemporalProperties temporalProperties;
+    private final IntegrationTestTemporalProperties integrationTestTemporalProperties;
 
 
     @Bean
     public WorkflowServiceStubs workflowServiceStubs() {
         return WorkflowServiceStubs.newServiceStubs(
-                WorkflowServiceStubsOptions.newBuilder().setTarget(temporalProperties.getHost()).build()
+                WorkflowServiceStubsOptions.newBuilder().setTarget(integrationTestTemporalProperties.getHost()).build()
         );
     }
 
     @Bean
-    public WorkflowClient workflowClient(WorkflowServiceStubs service) {
+    public WorkflowClient workflowClient(WorkflowServiceStubs service, CryptographyCodec cryptographyCodec) {
         return WorkflowClient.newInstance(service, WorkflowClientOptions.newBuilder()
-                .setNamespace(temporalProperties.getNamespace())
+                .setNamespace(integrationTestTemporalProperties.getNamespace())
+                .setDataConverter(new CodecDataConverter(
+                        DefaultDataConverter.newDefaultInstance(),
+                        Collections.singletonList(cryptographyCodec)
+                ))
                 .build());
     }
 }
