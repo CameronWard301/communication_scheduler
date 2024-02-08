35eecf422835ba6cad1fdcbcf5b76bb142b9f9dd
cameron-ward <cameron.ward@vodafone.com>
2024-02-08 17:56:54 +0000

    init schedule-api

---
+++ schedule-api/src/main/java/io/github/cameronward301/communication_scheduler/schedule_api/ScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.cameronward301.communication_scheduler.schedule_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApiApplication.class, args);
+    }
+
+}
---
+++ schedule-api/src/test/java/io/github/cameronward301/communication_scheduler/schedule_api/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cameronward301.communication_scheduler.schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
