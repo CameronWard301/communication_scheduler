94ca3092b256740bd71e4d994ffcafe6680df174
cameron-ward <cameron.ward@vodafone.com>
2024-02-18 17:56:47 +0000

    project init

---
+++ history-api/src/main/java/io/github/cameronward301/communication_scheduler/history_api/HistoryApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.cameronward301.communication_scheduler.history_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoryApiApplication.class, args);
+    }
+
+}
