ee07dbb1ab832d29eef7c03174b990aec659ba70
cameron-ward <cameron.ward@vodafone.com>
2024-02-18 11:52:39 +0000

    added schedule api integration tests

---
+++ integration-tests/src/test/java/io/github/cameronward301/communication_scheduler/integration_tests/model/schedule/CountDTO.java
@@ -0,0 +1,14 @@
+package io.github.cameronward301.communication_scheduler.integration_tests.model.schedule;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CountDTO {
+    private long total;
+}
