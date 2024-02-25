80e11d3aaa2c1f582b4ff8d76a15a51f3081b2f2
cameron-ward <cameron.ward@vodafone.com>
2024-02-25 11:45:39 +0000

    created home page

--- web-portal/src/App.tsx
+++ web-portal/src/App.tsx
@@ -1,21 +1,26 @@
-import {CssBaseline, ThemeProvider, createTheme} from '@mui/material'
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import {ThemeProvider, createTheme} from '@mui/material'
 import './App.css'
 import {ConfigProvider} from "./context/ConfigContext.tsx";
 import {SnackbarContextProvider} from "./context/SnackbarContext.tsx";
 import {RootStore} from "./stores/RootStore.tsx";
 import {StoreProvider} from "./context/StoreContext.tsx";
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import NavigationBar from "./components/navigation_bar";
+import AppRouter from "./AppRouter.tsx";
 
 const theme = createTheme({
   palette: {
-    mode: 'dark'
+    mode: 'dark',
+    primary: {
+      main: '#2E8BC0'
+    },
   }
 })
 
 const store = new RootStore();
 
-// noinspection JSUnusedGlobalSymbols
 function App() {
 
   return (
@@ -25,16 +30,11 @@ function App() {
           <ConfigProvider>
             <SnackbarContextProvider>
               <BrowserRouter>
+                <Box sx={{display: 'flex'}}>
               <CssBaseline/>
               <NavigationBar/>
-              <h1>Vite + React</h1>
-              <div className="card">
-
-
-              </div>
-              <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-              </p>
+                <AppRouter/>
+                </Box>
               </BrowserRouter>
             </SnackbarContextProvider>
           </ConfigProvider>
@@ -44,5 +44,4 @@ function App() {
   )
 }
 
-// noinspection JSUnusedGlobalSymbols
 export default App
