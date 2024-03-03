3c92668d2ef18f8cacf10c59f5175c0b0b813e68
cameron-ward <cameron.ward@vodafone.com>
2024-03-03 12:35:46 +0000

    ignore case when searching for fields

--- gateway-api/src/main/java/io/github/cameronward301/communication_scheduler/gateway_api/repository/GatewayRepository.java
+++ gateway-api/src/main/java/io/github/cameronward301/communication_scheduler/gateway_api/repository/GatewayRepository.java
@@ -4,10 +4,13 @@ import io.github.cameronward301.communication_scheduler.gateway_api.model.Gatewa
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface GatewayRepository extends MongoRepository<Gateway, String> {
     Page<Gateway> findAll(Pageable pageable);
 
-    Page<Gateway> findByFriendlyNameRegexAndEndpointUrlRegexAndDescriptionRegex(String friendlyName, String dateCreated, String description, Pageable pageable);
+    @Query("{'friendlyName': {$regex: ?0, $options: 'i'}, 'endpointUrl': {$regex: ?1, $options: 'i'}, 'description': {$regex: ?2, $options: 'i'}}")
+    Page<Gateway> findByFriendlyNameRegexAndEndpointUrlRegexAndDescriptionRegexIgnoreCase(String friendlyName, String endpointUrl, String description, Pageable pageable);
+
 
 }
