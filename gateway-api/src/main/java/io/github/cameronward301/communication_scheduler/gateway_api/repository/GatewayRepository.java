d6d09aca0d641121d92980698bd6dc6523d88280
cameron-ward <cameron.ward@vodafone.com>
2024-04-24 16:04:36 +0100

    repository method provided through interface

--- gateway-api/src/main/java/io/github/cameronward301/communication_scheduler/gateway_api/repository/GatewayRepository.java
+++ gateway-api/src/main/java/io/github/cameronward301/communication_scheduler/gateway_api/repository/GatewayRepository.java
@@ -7,10 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface GatewayRepository extends MongoRepository<Gateway, String> {
-    Page<Gateway> findAll(Pageable pageable);
-
-    @Query("{'friendlyName': {$regex: ?0, $options: 'i'}, 'endpointUrl': {$regex: ?1, $options: 'i'}, 'description': {$regex: ?2, $options: 'i'}}")
+   @Query("{'friendlyName': {$regex: ?0, $options: 'i'}, 'endpointUrl': {$regex: ?1, $options: 'i'}, 'description': {$regex: ?2, $options: 'i'}}")
     Page<Gateway> findByFriendlyNameRegexAndEndpointUrlRegexAndDescriptionRegexIgnoreCase(String friendlyName, String endpointUrl, String description, Pageable pageable);
-
-
 }
