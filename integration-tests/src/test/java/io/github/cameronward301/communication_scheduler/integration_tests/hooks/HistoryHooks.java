df99f2f6f396b83c2474630fc9ef425ad6947b85
cameron-ward <cameron.ward@vodafone.com>
2024-04-25 11:53:22 +0100

    ensure workflows are terminated and running in before and after hooks

--- integration-tests/src/test/java/io/github/cameronward301/communication_scheduler/integration_tests/hooks/HistoryHooks.java
+++ integration-tests/src/test/java/io/github/cameronward301/communication_scheduler/integration_tests/hooks/HistoryHooks.java
@@ -3,6 +3,7 @@ package io.github.cameronward301.communication_scheduler.integration_tests.hooks
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.cameronward301.communication_scheduler.integration_tests.gateway.Gateway;
+import io.github.cameronward301.communication_scheduler.integration_tests.model.JwtDTO;
 import io.github.cameronward301.communication_scheduler.integration_tests.model.schedule.ScheduleEntity;
 import io.github.cameronward301.communication_scheduler.integration_tests.world.World;
 import io.temporal.api.common.v1.WorkflowExecution;
@@ -15,11 +16,21 @@ import io.temporal.common.SearchAttributeKey;
 import io.temporal.common.SearchAttributes;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.net.URIBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Slf4j
 public class HistoryHooks {
@@ -27,13 +38,24 @@ public class HistoryHooks {
     private final World world;
     private final Gateway gateway;
     private final ScheduleEntity schedule;
+    private final RestTemplate restTemplate;
+    private final int maxAttempts = 10;
+    HttpHeaders httpHeaders = new HttpHeaders();
     List<String> workflowIds = new ArrayList<>();
 
-    public HistoryHooks(WorkflowClient workflowClient, World world, @Qualifier("gatewayDbModel") Gateway gateway, @Qualifier("scheduleEntity") ScheduleEntity scheduleEntity) {
+    @Value("${history-api.address}")
+    private String historyAPIUrl;
+
+
+
+    public HistoryHooks(WorkflowClient workflowClient, World world, @Qualifier("gatewayDbModel") Gateway gateway, @Qualifier("scheduleEntity") ScheduleEntity scheduleEntity, RestTemplate restTemplate, @Value("${auth-api.address}") String authAPIUrl) {
         this.workflowClient = workflowClient;
         this.world = world;
         this.gateway = gateway;
         this.schedule = scheduleEntity;
+        this.restTemplate = restTemplate;
+        httpHeaders.setContentType(APPLICATION_JSON);
+        httpHeaders.set("Authorization", "Bearer " + Objects.requireNonNull(restTemplate.postForEntity(authAPIUrl, List.of("HISTORY:READ"), JwtDTO.class).getBody()).getToken());
     }
 
     @Before(value = "@TerminateExistingWorkflows", order = 1)
@@ -43,6 +65,7 @@ public class HistoryHooks {
                 WorkflowStub workflow = workflowClient.newUntypedWorkflowStub(workflowExecution.getExecution().getWorkflowId());
                 workflow.terminate("test-termination");
                 checkWorkflowIsTerminated(workflowExecution.getExecution().getWorkflowId());
+                checkWorkflowCount(0);
             } catch (Exception e) {
                 log.debug(e.getMessage());
             }
@@ -70,6 +93,7 @@ public class HistoryHooks {
 
         }
         checkWorkflowsAreCreated();
+        checkWorkflowCount(5);
 
     }
 
@@ -84,6 +108,7 @@ public class HistoryHooks {
         world.setWorkflowExecution(execution);
         world.setWorkflowStub(workflow);
         checkWorkflowsAreCreated();
+        checkWorkflowCount(1);
     }
 
     @After("@RemoveTestWorkflows")
@@ -97,6 +122,7 @@ public class HistoryHooks {
             }
         }
         checkWorkflowsAreTerminated();
+        checkWorkflowCount(0);
     }
 
     private WorkflowStub getWorkflowStub(String workflowId) {
@@ -113,7 +139,7 @@ public class HistoryHooks {
 
     private void checkWorkflowsAreCreated() {
         for (String id : workflowIds) {
-            int attempts = 10;
+            int attempts = maxAttempts;
             while (attempts > 0) {
                 if (workflowClient.listExecutions("WorkflowId=\"" + id + "\"").filter(workflowExecutionMetadata -> workflowExecutionMetadata.getStatus()== WorkflowExecutionStatus.WORKFLOW_EXECUTION_STATUS_RUNNING).count() == 1) {
                     return;
@@ -128,16 +154,19 @@ public class HistoryHooks {
             }
             throw new RuntimeException("Workflow with id: " + id + " did not reach running state");
         }
+        //noinspection ConstantValue
+        checkWorkflowCount(workflowIds.size());
     }
 
     private void checkWorkflowsAreTerminated() {
         for (String id : workflowIds) {
             checkWorkflowIsTerminated(id);
         }
+        checkWorkflowCount(0);
     }
 
     private void checkWorkflowIsTerminated(String id){
-        int attempts = 10;
+        int attempts = maxAttempts;
         while (attempts > 0) {
             if (workflowClient.listExecutions("WorkflowId=\"" + id + "\"").noneMatch(workflowExecutionMetadata -> workflowExecutionMetadata.getStatus() == WorkflowExecutionStatus.WORKFLOW_EXECUTION_STATUS_RUNNING)) {
                 return;
@@ -151,5 +180,35 @@ public class HistoryHooks {
             attempts--;
         }
         throw new RuntimeException("Workflow with id: " + id + " did not reach terminated state");
+    }
+
+    private void checkWorkflowCount(int count) {
+        int attempts = maxAttempts;
+        while (attempts > 0) {
+            if (getRunningWorkflowCount() == count) {
+                return;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                log.error("Interrupted while waiting for workflows to terminate", e);
+                throw new RuntimeException("Interrupted while waiting for workflows to terminate");
+
+            }
+            attempts --;
+        }
+        throw new RuntimeException("Workflow count did not reach expected value in the history API");
+    }
+
+    private int getRunningWorkflowCount(){
+        try {
+            URIBuilder uriBuilder = new URIBuilder(historyAPIUrl + "/total");
+            uriBuilder.addParameter("userId", schedule.getScheduleOptions().getTypedSearchAttributes().get(SearchAttributeKey.forKeyword("userId")));
+            uriBuilder.addParameter("gatewayId", gateway.getId());
+            uriBuilder.addParameter("status", String.valueOf(WorkflowExecutionStatus.WORKFLOW_EXECUTION_STATUS_RUNNING_VALUE));
+            return Integer.parseInt(Objects.requireNonNull(restTemplate.exchange(uriBuilder.build(), GET, new HttpEntity<>(httpHeaders), Map.class).getBody()).get("total").toString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
