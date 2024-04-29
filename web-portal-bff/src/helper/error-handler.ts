95938262b5449f748174e7abe47cca0ff689fa0f
cameron-ward <cameron.ward@vodafone.com>
2024-04-29 21:59:57 +0100

    fix xss vulnerability

--- web-portal-bff/src/helper/error-handler.ts
+++ web-portal-bff/src/helper/error-handler.ts
@@ -3,6 +3,7 @@ import { AxiosError } from "axios";
 
 export const errorHandler = (response: Response, error: AxiosError) => {
   if (error.response) {
+    response.set('Content-Type', 'text/plain')
     response.status(error.response.status).send(error.response.data);
     console.debug("Error: ", error);
   } else if (error.request) {
